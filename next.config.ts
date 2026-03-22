69e15348d9618ed8639ced4eadead9466f6c734a
vaikunth-coder27 <59509590+vaikunth-coder27@users.noreply.github.com>
2026-03-22 21:59:54 +0000

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,9 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig: NextConfig = {
   reactCompiler: true,
   allowedDevOrigins: ['10.5.0.2'],
   output: "export",
-  basePath: isProd ? "/Vaikunth-portfolio" : "",
-  assetPrefix: isProd ? "/Vaikunth-portfolio/" : "",
   images: {
     unoptimized: true,
   },
